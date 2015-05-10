400818efbb1a4f92e9e566b17ec7bf46b1818337
Kenny Song <kenny.ysong@gmail.com>
2015-05-11 00:30:29 +0800

    Text to speech working! Fixed MakeCall.java

--- src/main/java/oop/project/MapApp.java
+++ src/main/java/oop/project/MapApp.java
@@ -137,6 +137,11 @@ public class MapApp extends Application implements MapComponentInitializedListen
      * Standard start method for JavaFX Applications
      */
     public void start(Stage stage) {
+        //test some of our fun classes
+        TextToSpeech.speak("Hello world!");
+        SMSSender.sendSMS("+12016321315", "Sent from MapApp.java.");
+        MakeCall.makeCall("+12016321315");
+
         //load BART routes and trajectories
         this.loadRoutes();
         this.loadTrajectories();
@@ -275,7 +280,7 @@ public class MapApp extends Application implements MapComponentInitializedListen
                 };
             }
         };
-        this.bartService.setPeriod(Duration.seconds(60));
+        this.bartService.setPeriod(Duration.seconds(5));
     }
 
     /**
@@ -296,7 +301,7 @@ public class MapApp extends Application implements MapComponentInitializedListen
             // See which trajctories are active at this time
             Coordinate currentCoord = trajectory.getPosition(this.trajectoryClock);
             if (currentCoord != null) {
-                System.out.println(trajectory.getTripId() + " " + currentCoord);
+                // System.out.println(trajectory.getTripId() + " " + currentCoord);
 
                 // Draw train on this map as a circle
                 // This is way too slow to function on my computer, but drawing Markers is okay
